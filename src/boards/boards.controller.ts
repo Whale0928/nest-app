c1a100faef93233a1cd81861895a4cb223a42cd9
KIM HYUN GI <rlagusrl928@gmail.com>
2023-12-27 01:02:57 +0900

    create board service

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { BoardsService } from './boards.service';
 
 @Controller('boards')
 export class BoardsController {
+  constructor(private boardsService: BoardsService) {} //접근제어자 선언시 암묵적으로 클래스 프로퍼티로 선언됨
+
   @Get()
   getAllBoards() {
-    return 'get all boards';
+    return this.boardsService.getAllBoards();
   }
 }
